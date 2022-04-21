d9039047dab016e1e418ee5fded875380f7a8cbd
SimonGreenhill <simon@simon.net.nz>
2022-04-21 22:06:57 +1200

    update

--- cldfbench_bouckaert_et_al2012.py
+++ cldfbench_bouckaert_et_al2012.py
@@ -10,23 +10,15 @@ class Dataset(phlorest.Dataset):
     def cmd_makecldf(self, args):
         self.init(args)
 
-        args.writer.add_summary(
-                self.raw_dir.read_tree('1219669IndoEuropean_2MCCtrees_annotated.tre'),
-                self.metadata,
-                args.log)
+        summary = self.raw_dir.read_tree(
+            '1219669IndoEuropean_2MCCtrees_annotated.tre')
+        args.writer.add_summary(summary, self.metadata, args.log)
 
-        posterior = self.sample(
-            self.remove_burnin(
-                self.raw_dir.read('IE2011_RelaxedCovarion_AllSingletonsGeo_Combined.trees.gz'),
-                1000),
-            detranslate=True,
-            as_nexus=True)
-
-        args.writer.add_posterior(
-            posterior.trees.trees,
-            self.metadata,
-            args.log)
-
+        posterior = self.raw_dir.read_trees(
+            'IE2011_RelaxedCovarion_AllSingletonsGeo_Combined.trees.gz',
+            burnin=1000, sample=1000, detranslate=True)
+        args.writer.add_posterior(posterior, self.metadata, args.log)
+        
         args.writer.add_data(
             self.raw_dir.read_nexus('IELex_Bouckaert2012.nex'),
             self.characters,
